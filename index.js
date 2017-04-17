e2042e45d3456843e223f719f021c43d2bc81229
Danny Vega <dannycvega@gmail.com>
2017-04-17 22:34:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function iterativeLog(array){array.forEach(callback2)}
+
+function callback2(element, index, array){console.log(index + ": " + element)}
+
+function iterate(callback){var array = ["horse"]; array.forEach(callback); return array;}
+
+function doToArray(array,cb){array.forEach(cb);}
